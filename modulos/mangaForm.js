bec608846fa3e842ff9e2b1e01cc0b8869e1f289
sparna28 <ismael28smr@gmail.com>
2023-06-26 18:56:25 +0200

    solucionado los margenes

--- modulos/mangaForm.js
+++ modulos/mangaForm.js
@@ -1,24 +1,25 @@
 const mangaForm = {
   template: `
-  <form @submit.prevent="submitForm" class="my-4">
-    <div class="mb-3">
-      <label for="nombre" class="form-label">Nombre:</label>
-      <input type="text" id="nombre" v-model="nombre" class="form-control">
-    </div>
-    
-    <div class="mb-3">
-      <label for="sinopsis" class="form-label">Sinopsis:</label>
-      <textarea id="sinopsis" v-model="sinopsis" class="form-control"></textarea>
-    </div>
-    
-    <div class="mb-3">
-      <label for="imagen" class="form-label">Imagen:</label>
-      <input type="file" id="imagen" @change="handleImageUpload" class="form-control">
-    </div>
-    
-    <button type="submit" class="btn btn-primary">Guardar</button>
-  </form>
-
+  <div class="container mt-4">
+    <form @submit.prevent="submitForm" class="my-4">
+      <div class="mb-3">
+        <label for="nombre" class="form-label">Nombre:</label>
+        <input type="text" id="nombre" v-model="nombre" class="form-control">
+      </div>
+      
+      <div class="mb-3">
+        <label for="sinopsis" class="form-label">Sinopsis:</label>
+        <textarea id="sinopsis" v-model="sinopsis" class="form-control"></textarea>
+      </div>
+      
+      <div class="mb-3">
+        <label for="imagen" class="form-label">Imagen:</label>
+        <input type="file" id="imagen" @change="handleImageUpload" class="form-control">
+      </div>
+      
+      <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+  </div>
   `,
   data() {
     return {
